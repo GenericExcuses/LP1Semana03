ad28c9c19c2a088e1ddc945c636995021ee73b4e
GenericExcuses <emaildotomasvaz@gmail.com>
2022-03-10 14:54:33 +0000

    PercorreString

---
+++ PercorreString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String userString;
+            char r;
+
+            Console.WriteLine("Qual é a string?");
+            userString = Console.ReadLine();
+            Console.WriteLine("Qual é o caracter?");
+            r = char.Parse(Console.ReadLine());
+
+            foreach(char c in userString)
+            {
+                if (c != r)
+                {
+
+                    Console.Write(c);
+
+                }
+
+            }
+
+
+        }
+    }
+}
